44e25e6aa054440eb204c263b210e4b3a6fef5f0
Ophélie Peyron <ophelie.peyron@gmail.com>
2020-12-09 10:40:35 +0100

    some changes on the index pois page

--- app/controllers/pois_controller.rb
+++ app/controllers/pois_controller.rb
@@ -4,8 +4,8 @@ class PoisController < ApplicationController
   def index
     @city = City.find(params[:city_id])
     @pois = @city.pois
-    @restaurants = @pois.where(category: "Restaurants").or(@pois.where(category: "Bars/ Vie nocturne")).first(3)
-    @activities = @pois.where(category: "Activités").first(3)
+    @restaurants = @pois.where(category: "Restaurants").or(@pois.where(category: "Bars/ Vie nocturne"))
+    @activities = @pois.where(category: "Activités")
     @unmissables = @pois.where(category: "Incontournables").first(3)
 
     # if params[:category].present?
