5bb6f3554d1b19e2355223059f17e9ef60c04c8f
Stephane Simcic <simcic.stephane@gmail.com>
2020-12-10 12:29:47 +0100

    Changed profile controller + view

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,11 +3,11 @@ class ProfilesController < ApplicationController
     @user = current_user
     # @favorite_city = City.joins(:favorites_pois).where()
     @favorite_cities = current_user.favorite_cities
-    @favorites_pois = current_user.favorite_pois
+    @favorites_pois = current_user.pois
     @last_reviews = current_user.reviews.last(3)
     @my_pois = Poi.joins(:reviews).where(reviews: { user: current_user })
 
-    @markers = @my_pois.geocoded.map do |poi|
+    @markers = @favorites_pois.geocoded.map do |poi|
       {
         lat: poi.latitude,
         lng: poi.longitude,
